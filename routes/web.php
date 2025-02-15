df7e5eec1abedcd24282951b65258a75249cbe1a
Larkaholic <orillosdominique@gmail.com>
2025-02-15 22:01:02 +0800

    removed welcome as first view

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index']);
 
 Route::resource('tasks', TaskController::class);
