60a04fd5f3680fadce79c5d1196cdadd5946caa2
Larkaholic <orillosdominique@gmail.com>
2025-02-15 21:22:43 +0800

    fix error 404

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel App</title>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';">
+</head>
+<body>
+</body>
+</html>
